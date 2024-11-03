7f19394d8b6fbd9d303c0b02842f245703bbe2ab
arjun-reboostify <reboostify@gmail.com>
2024-11-04 00:37:56 +0530

    f

--- src/pages/wisdom.tsx
+++ src/pages/wisdom.tsx
@@ -130,12 +130,12 @@ const QuoteBox = () => {
         
 
         <div className="flex justify-center items-center">
-          <div
-            className={`w-36 h-36 bg-cover bg-center rounded-full border-4 border-black shadow-lg ${
-              playing ? 'animate-spin-slow' : ''
-            }`}
-            style={{ backgroundImage: `url(${songsList[currentSong].cover})` }}
-          ></div>
+        <div
+      className={`w-36 h-36 bg-cover bg-center rounded-full border-4 border-black shadow-lg [animation-duration:4s] ${
+        playing ? 'animate-spin' : ''
+      }`}
+      style={{ backgroundImage: `url(${songsList[currentSong].cover})` }}
+    />
         </div>
 
         <div className="flex justify-center items-center gap-4 bg-gray-800 mt-4 rounded-xl p-4 shadow-lg">
